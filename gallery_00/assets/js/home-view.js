3f0282ded1ba3d607ef3c4f80702436730dfd573
Kristine Servia <32210828+kristineservia@users.noreply.github.com>
2026-08-03 18:52:20 -0400

    Create home-view.js file and export renderHomeView() function

---
+++ gallery_00/assets/js/home-view.js
@@ -0,0 +1,8 @@
+export function renderHomeView() {
+  const homeView = document.querySelector("#gallery__home-view");
+
+  homeView.innerHTML = `
+    <h3>Verse Memorization</h3>
+    <p>Home View Loaded!</p>
+  `;
+}
