ee237f483ee59872ba944f466c112f6095b00cb7
Kristine Servia <32210828+kristineservia@users.noreply.github.com>
2026-08-04 19:48:46 -0400

    Add router() function and hash-change event listener to index.js

--- gallery_00/assets/js/index.js
+++ gallery_00/assets/js/index.js
@@ -1,3 +1,20 @@
 import { renderHomeView } from "./home-view.js";
 
-renderHomeView();
+// ROUTER FUNCTION -App's URL HASH Navigation Method-
+
+function router() {
+  //the 'window.location.hash' line returns whatever comes after the # symbol on the browser's URL.
+  const route = window.location.hash;
+
+  if (route === "#/home") {
+    renderHomeView();
+  }
+
+  console.log(route);
+}
+
+//Run the Router function once when the page first loads
+router();
+
+//Run the Router function again when the page has a hash change
+window.addEventListener("hashchange", router);
